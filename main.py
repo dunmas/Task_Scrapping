a6dcc6f4f81639658910bd1dcdc1255bc7adf605
Danila <levafork@gmail.com>
2023-02-26 16:56:22 +0300

    Получил ссылки на все вакансии по заданным параметрам поиска, начал написание для обработки вакансии по найденной ссылке

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 from fake_headers import Headers
 import json
 
+# Города заданы в параметре 'area' в url
 prog_language = 'python'
-cities = ['Москва', 'Санкт-Петербург']
-keywords = ['Django', 'Flask']
+pages_count = 5
+keywords = ['django', 'flask']
 
 
 def get_headers():
@@ -13,7 +14,18 @@ def get_headers():
     return headers.generate()
 
 
+def vacancy_processing(link):
+    pass
+
+
 if __name__ == '__main__':
-    raw_resp = requests.get(f'https://spb.hh.ru/search/vacancy?text={prog_language}&area=1&area=2',
-                            headers=get_headers())
-    soup = BeautifulSoup(raw_resp.text, 'lxml')
+    for page in range(pages_count):
+        raw_resp = requests.get(f'https://spb.hh.ru/search/vacancy?text={prog_language}&area=1&area=2&page='
+                                f'{page}',
+                                headers=get_headers())
+        soup = BeautifulSoup(raw_resp.text, 'lxml')
+
+        vacancies = soup.find_all('div', class_='serp-item')
+        for vacancy in vacancies:
+            vacancy_link = vacancy.find('a', class_='serp-item__title').attrs.get('href')
+            vacancy_processing(vacancy_link)
